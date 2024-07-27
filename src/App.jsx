483b50f82b96b65cc5ef928bd042bb612f6459a7
subham jalan <subhamj13@gmail.com>
2024-07-28 00:35:37 +0530

    changes

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState , useRef} from 'react'
+import './App.css'
+
+function App() {
+  const [displayLogin, setLogin] = useState(false);
+  const username = useRef(null);
+  const password = useRef(null);
+  const [txt , setTxt] = useState(null);
+  const handler = (event)=>{
+    event.preventDefault();
+    const userName = username.current.value;
+    const pass =  password.current.value;
+    if(userName === 'user' && pass === 'password'){
+      setLogin(true);
+    }else{
+      setTxt("Invalid username or password");
+    }
+  }
+  return (
+    displayLogin ?
+      <div><p>Welcome, user!</p></div>
+      :
+      <div>
+        <h1>Login Page</h1>
+        <form >
+        <p>{txt}</p>
+        <div>
+        <label>UserName:</label>
+        <input ref={username} type="text" placeholder='username' required></input>
+        </div>
+        <div>
+        <label>Password:</label>
+        <input ref={password} type="text" placeholder='password' required></input>
+        </div>
+        <button onClick={handler} type='submit'>Submit</button>
+        </form>
+      </div>
+  )
+}
+
+export default App
